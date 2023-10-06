cd930ac42c5b4af9d7cd3913de5b5d983a692367
Jon Ander Beroz Basterretxea <jonanberoz2000@gmail.com>
2023-10-06 11:25:22 +0200

    Logo added

--- bazar-app/src/app/page.tsx
+++ bazar-app/src/app/page.tsx
@@ -1,8 +1,10 @@
 import SearchForm from "@/components/search-form";
+import Logo from "@/icons/logo";
 
 export default function Home() {
   return (
     <section className="text-center place-self-center">
+      <Logo />
       <h1 className="text-3xl font-semibold mb-4">Bazar Online</h1>
       <SearchForm showButton={true} />
     </section>
--- bazar-app/src/components/search-container.tsx
+++ bazar-app/src/components/search-container.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
+import Logo from "@/icons/logo";
 import SearchBar from "./search-form";
 
 export default function SearchContainer({children}: {children: React.JSX.Element}) {
   return (
-    <section className="flex flex-col gap-10 max-w-6xl w-full items-center p-4 ">
-      <header>
+    <section className="flex flex-col gap-10 max-w-6xl w-full items-center p-4">
+      <header className="flex gap-2 items-center">
+        <Link href="/" className="w-24 h-24 flex items-center">
+          <Logo />
+        </Link>
         <SearchBar />
       </header>
       {children}
